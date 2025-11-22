f6e3c6a0be56b872e091fd391bb17d9b637eca6e
Anslem Coelho <anslem1999@gmail.com>
2025-11-21 19:33:59 -0500

    Admin start

---
+++ admin/dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin</title>
+</head>
+<body>
+  <?php include('../includes/admin_header.php'); ?>
+  <h1>Welcome Admin!</h1>
+  
+  <a href="add_client.php">
+      Add Client
+  </a>
+
+  
+  <?php include('../includes/admin_footer.php'); ?>
+</body>
+</html>
